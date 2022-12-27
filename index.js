73e2b3115b237c0a86abf63dd051d37cffbdf0a6
tamara ramirez <tamanena@hotmail.com>
2022-12-27 12:33:45 -0300

    grid layout y flex

---
+++ index.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".navtoggle");
+const navMenu = document.querySelector(".navmenu");
+
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("navmenu_visible");
+});
